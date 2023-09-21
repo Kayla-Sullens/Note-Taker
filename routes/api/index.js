912857b608376426273460ed843704cfba0ada72
Kayla <sullenspartyof3@gmail.com>
2023-09-21 10:37:41 -0700

    Added post route

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,9 +1,12 @@
 let data = require('../../db/db.json');
 const router = require('express').Router();
-//const newNote = require('../../lib/notes');
 
 router.get('/notes', (req,res) => {
     res.json(data)
 });
+
+router.post('/notes', (req, res) => {
+    res.json(data)
+  });
 
 module.exports = router;
