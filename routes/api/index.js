92044a8f6a7f7f23429ce880880c98710edf7c4e
Kayla <sullenspartyof3@gmail.com>
2023-09-21 18:28:34 -0700

    Post api done in index.js

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,12 +1,21 @@
 let data = require('../../db/db.json');
 const router = require('express').Router();
-
-router.get('/notes', (req,res) => {
-    res.json(data)
+const fs = require('fs');
+router.get('/notes', (req, res) => {
+  res.json(data)
 });
 
 router.post('/notes', (req, res) => {
-    res.json(data)
+  const { title, text } = req.body;
+  const newNote = { title, text };
+  data.push(newNote);
+  fs.writeFile('../../db/db.json', JSON.stringify(data), (err) => {
+    if (err) {
+      res.status(500).json(err);
+    } else {
+      res.status(200).json(newNote);
+    }
   });
+});
 
 module.exports = router;
