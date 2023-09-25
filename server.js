20cc53ee830311dd765b45c3ca309f600c598577
Kayla <sullenspartyof3@gmail.com>
2023-09-25 13:18:57 -0700

    Commented out delete option

--- server.js
+++ server.js
@@ -12,6 +12,12 @@ app.use(express.json());
 app.use(express.static('public'));
 app.use("/", routes);
 
+//Working on getting the delete function to work
+// app.delete('/api/notes/:id', (req, res) => {
+//   const id = req.params;
+//   return res.send();
+// });
+
 app.listen(PORT, () => {
     console.log(`API server now on http://localhost:${PORT}`);
   });
