f4874e20e23a4d336ba181264daaf74f43b5185b
Kayla <sullenspartyof3@gmail.com>
2023-09-25 13:06:26 -0700

    Added npm package and working on delete option

--- routes/api/index.js
+++ routes/api/index.js
@@ -6,8 +6,12 @@ router.get('/notes', (req, res) => {
   res.json(data)
 });
 
+
+const generateUniqueId = require('generate-unique-id');
+
 router.post('/notes', (req, res) => {
   const { title, text } = req.body;
+  req.body.id = generateUniqueId();
   const newNote = { title, text };
   data.push(newNote);
   fs.writeFile('../../db/db.json', JSON.stringify(data), (err) => {
@@ -15,6 +19,19 @@ router.post('/notes', (req, res) => {
       res.status(500).json(err);
     } else {
       res.status(200).json(newNote);
+    }
+  });
+});
+
+router.delete('/notes', (req, res) => {
+  const { title, text } = req.body;
+  const deleteNote = { title, text };
+  data.push(deleteNote);
+  fs.writeFile('../../db/db.json', JSON.stringify(data), (err) => {
+    if (err) {
+      res.status(500).json(err);
+    } else {
+      res.status(200).json(deleteNote);
     }
   });
 });
