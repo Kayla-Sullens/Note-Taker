757aed6addad87a531da4a758c861c102453cd1f
Kayla <sullenspartyof3@gmail.com>
2023-09-25 18:21:42 -0700

    Fixed the notes display and being clickable

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,20 +1,24 @@
-let data = require('../../db/db.json');
+//let data = require('../../db/db.json');
 const router = require('express').Router();
 const fs = require('fs');
-
-router.get('/notes', (req, res) => {
-  res.json(data)
-});
-
-
 const generateUniqueId = require('generate-unique-id');
 
+const getData = () => {
+  const data = fs.readFileSync('db/db.json', {encoding: "utf8"});
+  return JSON.parse(data);
+}
+
+router.get('/notes', (req, res) => {
+  const data = getData();
+  res.json(data);
+});
+
 router.post('/notes', (req, res) => {
+  const data = getData();
   const { title, text } = req.body;
-  req.body.id = generateUniqueId();
-  const newNote = { title, text };
+  const newNote = { title, text, id: generateUniqueId() };
   data.push(newNote);
-  fs.writeFile('../../db/db.json', JSON.stringify(data), (err) => {
+  fs.writeFile('db/db.json', JSON.stringify(data), (err) => {
     if (err) {
       res.status(500).json(err);
     } else {
@@ -23,17 +27,18 @@ router.post('/notes', (req, res) => {
   });
 });
 
-router.delete('/notes', (req, res) => {
-  const { title, text } = req.body;
-  const deleteNote = { title, text };
-  data.push(deleteNote);
-  fs.writeFile('../../db/db.json', JSON.stringify(data), (err) => {
-    if (err) {
-      res.status(500).json(err);
-    } else {
-      res.status(200).json(deleteNote);
-    }
-  });
-});
+// use an array, pull in data, and write file again
+// router.delete('/notes/:id', (req, res) => {
+//   const { title, text } = req.body;
+//   const deleteNote = { title, text };
+//   data.push(deleteNote);
+//   fs.writeFile('../../db/db.json', JSON.stringify(data), (err) => {
+//     if (err) {
+//       res.status(500).json(err);
+//     } else {
+//       res.status(200).json(deleteNote);
+//     }
+//   });
+// });
 
 module.exports = router;
